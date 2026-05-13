c011bbce7b32e5fc8d2f59f68fb31baca99dac9b
luzass <lucasctdebritto@gmail.com>
2026-05-13 18:19:01 -0300

    remove tailwind cdn e adiciona css compilado

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./medicina/**/*.html",
+    "./*.js",
+    "./src/**/*.{js,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
